1a9e2d20310dd6c3073b921399758082e062c2e0
Siva Gogineni <siva9202@yahoo.com>
2021-12-09 17:11:07 -0500

    Update Siva_kNN.py
    Labeled x-axis

--- Siva_kNN.py
+++ Siva_kNN.py
@@ -203,8 +203,8 @@ plt.ylim(X2.min(), X2.max())
 for i, j in enumerate(np.unique(y_set)):
     plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1], c = ListedColormap(('red', 'green','blue'))(i), label = j)
 plt.title('K-NN (Test set)')
-#plt.xlabel('Alcohol')
-#plt.ylabel('Chlorides')
+plt.xlabel('Alcohol')
+plt.ylabel('Chlorides')
 plt.legend()
 plt.show()
 #%%
